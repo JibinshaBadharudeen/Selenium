75f0464691ad784938931f437cbbb65c0387ae26
Jibinsha <jibinshaeb@gmail.com>
2024-12-04 10:51:21 +0000

    done

--- NewTest.java
+++ NewTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("Hi mam");
+	  System.out.println("Hi mam done");
 	  System.out.println("Hi");
   }
 }
