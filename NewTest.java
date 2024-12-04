5c61425b659a5ae72cdf24cdaa259bfe7b22b641
Jibinsha <jibinshaeb@gmail.com>
2024-12-04 10:56:18 +0000

    ewrf

--- NewTest.java
+++ NewTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("Hi mam done");
+	  System.out.println("Hi mam donen jnj");
 	  System.out.println("Hi");
   }
 }
