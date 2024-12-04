bcc07d23aa6cb6b0ffe0353021c5e15292b92cde
Jibinsha <jibinshaeb@gmail.com>
2024-12-04 11:10:04 +0000

    adc

--- NewTest.java
+++ NewTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("Hi magftm doneefn jndsssfvj hvgh");
+	  System.out.println("Hi magftm dondsveefn jndsssfvj hvgh");
 	  System.out.println("Hi");
   }
 }
